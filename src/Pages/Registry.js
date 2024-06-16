54e2f97d857c2aba92fd34b593cb417cdf8957fa
Daniel Gunther <daniel.gunther@gmail.com>
2024-06-16 15:40:55 -0400

    [RSIT-007] Added registry page

--- src/Pages/Registry.js
+++ src/Pages/Registry.js
@@ -1,16 +1,31 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import House from "../resources/House.jpg";
+import vacation from "../resources/vacation.jpg";
 
 function Registry() {
   const textStyle =
-    "container mx-auto p-4 justify-center w-full lg:w-1/3 my-40";
+    "container mx-auto p-4 justify-center w-full lg:w-1/3 lg:my-20";
 
   return (
     <>
       <Header />
       <div class={textStyle}>
+        <img class="h-auto max-w-full my-4 rounded-xl" src={vacation} alt="" />
         <h3 class="text-center mb-1 text-5xl font-normal leading-none text-[#D3AF5E]">
-          Coming Soon
+          Honeymoon Fund
+        </h3>
+        <h3 id="textbox" class="text-center text-xl text-gray-400">
+          Help us pay for our honeymoon!
+        </h3>
+      </div>
+      <div class={textStyle}>
+        <img class="h-auto max-w-full rounded-xl my-4" src={House} alt="" />
+        <h3 class="text-center mb-1 text-5xl font-normal leading-none text-[#D3AF5E]">
+          Renovation Fund
+        </h3>
+        <h3 id="textbox" class="text-center text-xl text-gray-400">
+          Help us pay for our home renovations!
         </h3>
       </div>
       <Footer />
