76f6102b77289c0f26f376e94d986f7ad6303b4b
Daniel Gunther <daniel.gunther@gmail.com>
2024-06-16 16:22:53 -0400

    [RSIT-008] Added registry page

--- src/Pages/Registry.js
+++ src/Pages/Registry.js
@@ -18,6 +18,16 @@ function Registry() {
         <h3 id="textbox" class="text-center text-xl text-gray-400">
           Help us pay for our honeymoon!
         </h3>
+        <h3 id="textbox" class="text-center text-xl text-gray-400">
+          If you wish to leave a cash gift for this fund, there are two options:
+        </h3>
+        <h3 id="textbox" class="text-center text-xl text-gray-400">
+          1. You may leave it on the day of the wedding.
+        </h3>
+        <h3 id="textbox" class="text-center text-xl text-gray-400">
+          2. Send an e-transfer to: daniel.gunther@gmail.com, with a message
+          including "Honeymoon Fund".
+        </h3>
       </div>
       <div class={textStyle}>
         <img class="h-auto max-w-full rounded-xl my-4" src={House} alt="" />
@@ -26,6 +36,16 @@ function Registry() {
         </h3>
         <h3 id="textbox" class="text-center text-xl text-gray-400">
           Help us pay for our home renovations!
+        </h3>
+        <h3 id="textbox" class="text-center text-xl text-gray-400">
+          If you wish to leave a cash gift for this fund, there are two options:
+        </h3>
+        <h3 id="textbox" class="text-center text-xl text-gray-400">
+          1. You may leave it on the day of the wedding.
+        </h3>
+        <h3 id="textbox" class="text-center text-xl text-gray-400">
+          2. Send an e-transfer to: daniel.gunther@gmail.com, with a message
+          including "Renovation Fund".
         </h3>
       </div>
       <Footer />
