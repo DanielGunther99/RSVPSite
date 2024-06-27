058242881b043d5d0247788c205d778b2cbf0b67
Daniel Gunther <daniel.gunther@gmail.com>
2024-06-26 21:20:44 -0400

    no message

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://ec2-3-88-158-175.compute-1.amazonaws.com:5000";
+const baseURL = "http://ec2-3-88-158-175.compute-1.amazonaws.com:5000";
 
 const app = axios.create({
   baseURL,
